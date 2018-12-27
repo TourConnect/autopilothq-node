265b8f46fd694c2b6ff52943dda3d73ee01a3043
Seth McGuinness <seth.mcguinness@yahoo.com>
2018-12-27 10:32:08 -0600

    add bulkAdd method

--- src/contact.js
+++ src/contact.js
@@ -2,8 +2,9 @@ class Contact {
  constructor(client) {
    this.client = client;
  }
- add = (obj) => this.client.post('/contact', obj);
- update = (obj) => this.client.post('/contact', obj);
+ add = (obj) => this.client.post('/contact', { contact: obj });
+ bulkAdd = (array) => this.client.post('/contact', { contacts: array });
+ update = (obj) => this.client.post('/contact', { contact: obj });
  get = (contactIdOrEmail) => this.client.get(`/contact/${contactIdOrEmail}`);
  delete = (contactIdOrEmail) => this.client.delete(`/contact/${contactIdOrEmail}`);
  unsubscribe = (contactIdOrEmail) => this.client.post(`/contact/${contactIdOrEmail}/unsubscribe`, obj);
