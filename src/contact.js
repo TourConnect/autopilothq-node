8acf2fd4d88f2148d405022ff61243e04f9e9692
Seth McGuinness <seth.mcguinness@yahoo.com>
2018-12-27 12:38:07 -0600

    fix bulkadd endpoint

--- src/contact.js
+++ src/contact.js
@@ -3,7 +3,7 @@ class Contact {
    this.client = client;
  }
  add = (obj) => this.client.post('/contact', { contact: obj });
- bulkAdd = (array) => this.client.post('/contact', { contacts: array });
+ bulkAdd = (array) => this.client.post('/contacts', { contacts: array });
  update = (obj) => this.client.post('/contact', { contact: obj });
  get = (contactIdOrEmail) => this.client.get(`/contact/${contactIdOrEmail}`);
  delete = (contactIdOrEmail) => this.client.delete(`/contact/${contactIdOrEmail}`);
