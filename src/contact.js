83538f6ec3b29bebe27903010c92cbb681b898e0
Seth McGuinness <seth.mcguinness@yahoo.com>
2018-12-29 00:33:47 -0600

    completed contact API

--- src/contact.js
+++ src/contact.js
@@ -1,13 +1,57 @@
+import parse from './utils/parse';
+
 class Contact {
- constructor(client) {
-   this.client = client;
- }
- add = (obj) => this.client.post('/contact', { contact: obj });
- bulkAdd = (array) => this.client.post('/contacts', { contacts: array });
- update = (obj) => this.client.post('/contact', { contact: obj });
- get = (contactIdOrEmail) => this.client.get(`/contact/${contactIdOrEmail}`);
- delete = (contactIdOrEmail) => this.client.delete(`/contact/${contactIdOrEmail}`);
- unsubscribe = (contactIdOrEmail) => this.client.post(`/contact/${contactIdOrEmail}/unsubscribe`, obj);
+  constructor(client) {
+    this.client = client;
+
+    this.standardFields = [
+      'contact_id',
+      'Email',
+      'Twitter',
+      'FirstName',
+      'LastName',
+      'Salutation',
+      'Company',
+      'NumberOfEmployees',
+      'Title',
+      'Industry',
+      'Phone',
+      'MobilePhone',
+      'Fax',
+      'Website',
+      'MailingStreet',
+      'MailingCity',
+      'MailingState',
+      'MailingPostalCode',
+      'MailingCountry',
+      'owner_name',
+      'LeadSource',
+      'Status',
+      'LinkedIn',
+      'unsubscribed',
+      'custom',
+      '_autopilot_session_id',
+      '_autopilot_list',
+      'notify',
+    ];
+  }
+
+  add = obj => this.client.post('/contact', { contact: parse(this.standardFields, obj) });
+
+  all = (bookmark = null) => this.client.get(`/contacts${bookmark ? `/${bookmark}` : ''}`);
+
+  bulkAdd = array => this.client.post('/contacts', { contacts: array.map(obj => parse(this.standardFields, obj)) });
+
+  update = obj => this.client.post('/contact', { contact: parse(this.standardFields, obj) });
+
+  get = contactIdOrEmail => this.client.get(`/contact/${contactIdOrEmail}`);
+
+  delete = contactIdOrEmail => this.client.delete(`/contact/${contactIdOrEmail}`);
+
+  unsubscribe = contactIdOrEmail => this.client.post(`/contact/${contactIdOrEmail}/unsubscribe`);
+
+  // addToList = (contactIdOrEmail, listId) => this.client.post(`/list/${listId}/contact/${contactIdOrEmail}`)
+  // isOnList = (contactIdOrEmail, )
 }
 
 export default Contact;
--- test/contact.test.js
+++ test/contact.test.js
@@ -6,64 +6,84 @@ const autopilotApi = nock('https://api2.autopilothq.com/v1/');
 const autopilot = new Autopilot('key');
 
 describe('contacts', () => {
-  beforeEach(() => {
-    jest.setTimeout(2000);
-  });
   it('should add a contact', () => {
     expect.assertions(1);
-    autopilotApi.post('/contact').reply(200, { contact_id: "person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7" });
+    autopilotApi.post('/contact').reply(200, { contact_id: 'person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7' });
 
-    const request = autopilot.contact.add({ email: 'test@example.com'})
-    return expect(request).resolves.toEqual({ contact_id: 'person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7' })
-  })
+    const request = autopilot.contact.add({ email: 'test@example.com' });
+    return expect(request).resolves.toEqual({ contact_id: 'person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7' });
+  });
   it('should bulk add a contact', () => {
     expect.assertions(1);
     autopilotApi.post('/contacts').reply(200, {
-      "contact_ids": [
-        "person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7",
-        "person_9EAF39E4-9AEC-4134-964A-D9D8D5416AAA"
+      contact_ids: [
+        'person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7',
+        'person_9EAF39E4-9AEC-4134-964A-D9D8D5416AAA',
       ],
-      "email_contact_map": {
-        "chris@autopilothq.com": "person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7",
-        "peter@autopilothq.com": "person_9EAF39E4-9AEC-4134-964A-D9D8D5416AAA"
-      }
-});
+      email_contact_map: {
+        'chris@autopilothq.com': 'person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7',
+        'peter@autopilothq.com': 'person_9EAF39E4-9AEC-4134-964A-D9D8D5416AAA',
+      },
+    });
 
-    const request = autopilot.contact.bulkAdd([{ Email: 'chris@autopilothq.com' }, { Email: 'peter@autopilothq.com' }])
+    const request = autopilot.contact.bulkAdd([{ Email: 'chris@autopilothq.com' }, { Email: 'peter@autopilothq.com' }]);
     return expect(request).resolves.toEqual({
-      "contact_ids": [
-        "person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7",
-        "person_9EAF39E4-9AEC-4134-964A-D9D8D5416AAA"
+      contact_ids: [
+        'person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7',
+        'person_9EAF39E4-9AEC-4134-964A-D9D8D5416AAA',
       ],
-      "email_contact_map": {
-        "chris@autopilothq.com": "person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7",
-        "peter@autopilothq.com": "person_9EAF39E4-9AEC-4134-964A-D9D8D5416AAA"
-      }
-    })
-  })
+      email_contact_map: {
+        'chris@autopilothq.com': 'person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7',
+        'peter@autopilothq.com': 'person_9EAF39E4-9AEC-4134-964A-D9D8D5416AAA',
+      },
+    });
+  });
   it('should update a contact', () => {
     expect.assertions(1);
-    autopilotApi.post('/contact').reply(200, { contact_id: "person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7" });
+    autopilotApi.post('/contact').reply(200, { contact_id: 'person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7' });
 
-    const request = autopilot.contact.update({ email: 'test@example.com'})
+    const request = autopilot.contact.update({ email: 'test@example.com' });
 
-    return expect(request).resolves.toEqual({ contact_id: 'person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7' })
-  })
+    return expect(request).resolves.toEqual({ contact_id: 'person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7' });
+  });
   it('should get a contact', () => {
     expect.assertions(1);
     autopilotApi.get(/\/contact\/\S*$/).reply(200, { contact_id: 'person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7' });
 
-    const request = autopilot.contact.get('test@example.com')
+    const request = autopilot.contact.get('test@example.com');
 
     return expect(request).resolves.toEqual({ contact_id: 'person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7' });
+  });
+  it('should unsubscribe a contact', () => {
+    expect.assertions(1);
+    autopilotApi.post(/\/contact\/\S*\/unsubscribe$/).reply(200, {});
 
-  })
+    const request = autopilot.contact.unsubscribe('test@example.com');
+
+    return expect(request).resolves.toEqual({});
+  });
   it('should delete a contact', () => {
     expect.assertions(1);
-    autopilotApi.delete(/\/contact\/\S*$/).reply(200, { contact_id: 'person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7' });
+    autopilotApi.delete(/\/contact\/\S*$/).reply(200, {});
 
-    const request = autopilot.contact.delete('test@example.com')
+    const request = autopilot.contact.delete('test@example.com');
 
-    return expect(request).resolves.toEqual({ contact_id: 'person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7' });
-  })
+    return expect(request).resolves.toEqual({});
+  });
+  it('should list all contacts', () => {
+    expect.assertions(1);
+    autopilotApi.get(/\/contacts$/).reply(200, { contacts: [], total_contacts: 0 });
+
+    const request = autopilot.contact.all();
+
+    return expect(request).resolves.toEqual({ contacts: [], total_contacts: 0 });
+  });
+  it('should list all contacts with bookmark', () => {
+    expect.assertions(1);
+    autopilotApi.get(/\/contacts\/?\S*$/).reply(200, { contacts: [], total_contacts: 0 });
+
+    const request = autopilot.contact.all('person_9EAF39E4-9AEC-4134-964A-D9D8D54162E7');
+
+    return expect(request).resolves.toEqual({ contacts: [], total_contacts: 0 });
+  });
 });
