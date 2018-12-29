8f67f638dbd9d553f92df64b055e3271ec9f844a
Seth McGuinness <seth.mcguinness@yahoo.com>
2018-12-29 00:32:43 -0600

    handle request options.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Client from './client';
 import Contact from './contact';
+import List from './list';
 
 class Autopilot {
   constructor(apiKey, options) {
@@ -8,9 +9,11 @@ class Autopilot {
     const client = new Client({
       baseUrl: 'https://api2.autopilothq.com/v1/',
       headers: { autopilotapikey: apiKey },
+      ...options,
     });
 
     this.contact = new Contact(client);
+    this.list = new List(client);
   }
 }
 
