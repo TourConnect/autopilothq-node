ebc8a006c98731287a13cadcfb8009e446890d06
Seth McGuinness <seth.mcguinness@yahoo.com>
2018-12-29 00:31:50 -0600

    code cleanup

--- src/client.js
+++ src/client.js
@@ -10,7 +10,7 @@ class Client {
     const defaultArgs = {
       headers: { 'Content-Type': 'application/json' },
       json: true,
-    }
+    };
     return request(merge({}, this.requestOpts, defaultArgs, args));
   }
 
